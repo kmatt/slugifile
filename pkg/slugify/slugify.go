b7d7f66793da5fab98bf43c0164501e410479b1d
kmatt <matt@kmatt.dev>
2025-05-20 15:42:54 -0400

    Allowed characters

--- pkg/slugify/slugify.go
+++ pkg/slugify/slugify.go
@@ -21,15 +21,17 @@ func Slugify(input string, lowercase bool) string {
 	}
 	slug = strings.TrimSpace(slug)
 
+	// Multiple dots are left in place for files with multiple extensions (".tar.gz")
 	var sets = []Replacement{
 		{To: "", From: regexp.MustCompile("'")},
 		{To: "-", From: regexp.MustCompile(" - ")},
 		{To: "_", From: regexp.MustCompile(" _ ")},
-		{To: separator, From: regexp.MustCompile(`[^-\w\s{}]+`)}, // Remove all special characters except -
-		{To: separator, From: regexp.MustCompile(`[/^ /]`)},      // Replace all spaces with separator
+		{To: separator, From: regexp.MustCompile(`[^-\.\w\s{}]+`)}, // Remove all special characters except dash and dot
+		{To: separator, From: regexp.MustCompile(`[/^ /]`)},        // Replace all spaces with separator
 		{To: separator, From: regexp.MustCompile("`")},
 		{To: separator, From: regexp.MustCompile(`[\s]+`)}, // Replace all spaces with separator
 		{To: ".", From: regexp.MustCompile(`[.]+`)},        // Replace multiple dots with separator
+		{To: separator, From: regexp.MustCompile(`-+`)},    // Collapse multiple dashes
 		{To: "", From: regexp.MustCompile(`[.]$`)},         // Remove last dot
 		{To: "", From: regexp.MustCompile(`^[.]`)},         // Remove first dot
 	}
