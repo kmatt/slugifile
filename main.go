0be77a4cd553094845f8224ca1731d39482e62e7
kmatt <matt@kmatt.dev>
2025-05-20 13:58:24 -0400

    Usage

--- main.go
+++ main.go
@@ -25,6 +25,36 @@ import (
 	"github.com/ewilan-riviere/slugifier/pkg/utils"
 )
 
+
+// Get the file path from the command-line arguments
+func getPath() string {
+	// Check if at least one command-line argument is provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: slugifier <file-path>")
+		return ""
+	}
+
+	// The first argument (os.Args[0]) is the program name
+	filePath := os.Args[1]
+
+	if strings.HasPrefix(filePath, "-") {
+		filePath = os.Args[2]
+	}
+
+	// Check if the file exists
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("File '%s' does not exist.\n", filePath)
+		} else {
+			fmt.Println("Error:", err)
+		}
+		return ""
+	}
+
+	return filePath
+}
+
 func main() {
 	var showHelp = false
 	var verbose = false
@@ -35,11 +65,9 @@ func main() {
 	flag.BoolVar(&lowercase, "l", false, "Lowercase mode")
 
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Welcome to slugifier!")
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] path/to/file-or-directory\n", os.Args[0])
 		fmt.Fprintln(os.Stderr, "")
-		fmt.Fprintf(os.Stderr, "Example: `slugifier path/to/file` or `slugifier path/to/directory`")
-		// fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MESSAGE\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, "")
+		flag.PrintDefaults()
 	}
 
 	flag.Parse()
@@ -70,33 +98,4 @@ func main() {
 
 	renamer.Execute(filePath, maxLevel, lowercase)
 	fmt.Println("Done!")
-}
-
-// Get the file path from the command-line arguments
-func getPath() string {
-	// Check if at least one command-line argument is provided
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: slugifier <file-path>")
-		return ""
-	}
-
-	// The first argument (os.Args[0]) is the program name
-	filePath := os.Args[1]
-
-	if strings.HasPrefix(filePath, "-") {
-		filePath = os.Args[2]
-	}
-
-	// Check if the file exists
-	_, err := os.Stat(filePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("File '%s' does not exist.\n", filePath)
-		} else {
-			fmt.Println("Error:", err)
-		}
-		return ""
-	}
-
-	return filePath
 }
